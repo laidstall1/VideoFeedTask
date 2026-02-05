2561af3be223eb37c1409b01f784fa2c46ee67cd
oluwatoni-michael <laidstall@gmail.com>
2026-02-05 12:26:00 +0100

    Initial Commit

---
+++ VideoFeedTask/VideoFeedTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoFeedTaskApp.swift
+//  VideoFeedTask
+//
+//  Created by DIGITAL VENTURES on 05/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct VideoFeedTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
